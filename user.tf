d50f4b28a7bddf95495df74b15f1a3096f1a09a7
ajaysinghrathor <ajay.21.rathor@gmail.com>
2023-05-28 12:11:31 +0530

    ss

--- user.tf
+++ user.tf
@@ -1,3 +1,7 @@
+data "azurerm_client_config" "azure-cli-2023-05-26-17-40-23" {
+    id = "azure-cli-2023-05-26-17-40-23"
+}
+
 data "azurerm_subscription" "primary" {
 }
 resource "azuread_user" "startstopserver" {
@@ -9,7 +13,7 @@ resource "azuread_user" "startstopserver" {
 
 resource "azurerm_role_definition" "startstopwebserver" {
     name = "startstopwebserver"
-    #role_definition_id = "00000000-0000-0000-0000-000000000000"
+    role_definition_id = "00000000-0000-0000-0000-000000000000"
 
     scope = data.azurerm_subscription.primary.id
 
@@ -22,5 +26,11 @@ resource "azurerm_role_definition" "startstopwebserver" {
     assignable_scopes = [
         data.azurerm_subscription.primary.id
     ]
+}
 
+resource "azurerm_role_assignment" "assign_role" {
+  name = "00000000-0000-0000-0000-000000000000"
+  scope = data.azurerm_subscription.primary.id
+  role_definition_id = azurerm_role_definition.startstopwebserver.role_definition_id
+  principal_id = data.azurerm_client_config.azure-cli-2023-05-26-17-40-23.object_id
 }
